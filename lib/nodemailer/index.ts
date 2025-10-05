3f15712e115c2606b3c998b4eeb546ecf153fef2
HarshSahuu1234 <harshsahuu162@gmail.com>
2025-10-05 12:49:42 +0530

    errors

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -1,12 +1,13 @@
 "use server"
-import nodemailer from "nodemailer"
-import { EmailProductInfo, NotificationType, EmailContent } from "@/types"
-import productDetails from "@/app/products/[id]/page"
+
+import { EmailContent, EmailProductInfo, NotificationType } from '@/types';
+import nodemailer from 'nodemailer';
+
 const Notification = {
-    WELCOME: 'WELCOME',
-    CHANGE_OF_STOCK: 'CHANGE_OF_STOCK',
-    LOWEST_PRICE: 'LOWEST_PRICE',
-    THRESHOLD_MET: 'THRESHOLD_MET',
+  WELCOME: 'WELCOME',
+  CHANGE_OF_STOCK: 'CHANGE_OF_STOCK',
+  LOWEST_PRICE: 'LOWEST_PRICE',
+  THRESHOLD_MET: 'THRESHOLD_MET',
 }
 
 export async function generateEmailBody(
@@ -80,17 +81,14 @@ export async function generateEmailBody(
 }
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.office365.com',
-  port: 587,
-  secure: false,
+  pool: true,
+  service: 'hotmail',
+  port: 2525,
   auth: {
     user: 'harshsahu9234@outlook.com',
     pass: process.env.EMAIL_PASSWORD,
   },
-  tls: {
-    ciphers: "SSLv3",
-    rejectUnauthorized: false,
-  },
+  maxConnections: 1
 })
 
 export const sendEmail = async (emailContent: EmailContent, sendTo: string[]) => {
@@ -101,12 +99,11 @@ export const sendEmail = async (emailContent: EmailContent, sendTo: string[]) =>
     subject: emailContent.subject,
   }
 
-  try {
-    const info = await transporter.sendMail(mailOptions);
-    console.log("✅ Email sent:", info.response);
-  } catch (error) {
-    console.error("❌ Email sending failed:", error);
-  }
+  transporter.sendMail(mailOptions, (error: any, info: any) => {
+    if(error) return console.log(error);
+    
+    console.log('Email sent: ', info);
+  })
 }
 
 // sendGrid pass - Z.NTsJ9rbzAyV5p
